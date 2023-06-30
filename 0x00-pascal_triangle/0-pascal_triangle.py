4614f734c262aa283f532599c1d872c11453cce8
Emma0147 <mromotoriogunzabere@gmail.com>
2023-06-30 07:38:05 -0700

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,20 +5,20 @@ Pascal's Triangle
 
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []
+    triangle = []
 
-    triangle = [[1]]
+    if n <= 0:
+        return triangle
+
+    triangle.append([1])
 
     for i in range(1, n):
         row = [1]
-        prev_row = triangle[i - 1]
 
         for j in range(1, i):
-            row.append(prev_row[j - 1] + prev_row[j])
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
 
         row.append(1)
         triangle.append(row)
 
     return triangle
-
